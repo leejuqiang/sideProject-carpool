05ea0126945556567562a595e10870f24af4c15b
leejuqiang <leejuqiang@gmail.com>
2018-10-27 15:56:50 -0700

    add basic function

--- server.js
+++ server.js
@@ -2,28 +2,6 @@ var moduleHttps = require("https");
 var moduleFs = require("fs");
 var moduleUrl = require("url");
 var moduleLog = require("log4js");
-var l4jLogger;
-
-const options = { "key": moduleFs.readFileSync("./privatekey.pem"), "cert": moduleFs.readFileSync("cert.pem") };
-
-function requestFile(path, res) {
-    if (path === "/") {
-        path = "/index.html";
-    }
-    path = "/frontEnd" + path;
-    moduleFs.readFile("." + path, function (error, data) {
-        if (error) {
-            return serverResponse(res, 404, "404 Not Found");
-        }
-        return serverResponse(res, 200, data);
-    });
-}
-
-function serverResponse(res, code, body) {
-    res.writeHead(code, { "Content-Type": "text/html" });
-    res.write(body);
-    return res.end();
-}
 
 moduleLog.configure({
     "appenders": {
@@ -42,8 +20,31 @@ moduleLog.configure({
         }
     }
 });
-l4jLogger = moduleLog.getLogger("default");
-exports.logger = l4jLogger;
+exports.logger = moduleLog.getLogger("default");
+
+var opMap = {}
+
+const options = { "key": moduleFs.readFileSync("./privatekey.pem"), "cert": moduleFs.readFileSync("cert.pem") };
+
+function requestFile(path, res) {
+    if (path === "/") {
+        path = "/index.html";
+    }
+    path = "/frontEnd" + path;
+    moduleFs.readFile("." + path, function (error, data) {
+        if (error) {
+            return exports.respond(res, 404, "404 Not Found");
+        }
+        return exports.respond(res, 200, data);
+    });
+}
+
+exports.respond = function (res, code, body) {
+    res.writeHead(code, { "Content-Type": "text/html" });
+    res.write(body);
+    return res.end();
+}
+
 require("./database").connect(onConnectDB);
 
 function onConnectDB(error, db) {
@@ -51,7 +52,12 @@ function onConnectDB(error, db) {
         exports.logger.info("connected db, starting server");
         moduleHttps.createServer(options, function (req, res) {
             var param = moduleUrl.parse(req.url, true);
-            requestFile(param.pathname, res);
+            var op = opMap[param.pathname];
+            if (op != undefined) {
+                op.onRequest(req, res);
+            } else {
+                requestFile(param.pathname, res);
+            }
         }).listen(8080);
     } else {
         exports.logger.error("can't connect to db, error:" + error);
