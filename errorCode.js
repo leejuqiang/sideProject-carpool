6e9f3df03ffa394f083db300dfc5e0d4a3b48f4b
pi31415926 <st271828@sina.com>
2018-11-10 05:56:02 -0800

    sloved databaseError issue which stop server start

--- errorCode.js
+++ errorCode.js
@@ -1,3 +1,3 @@
 exports.ok = {"code": 0, "error": "ok"};
 exports.noSuchUser = {"code": 1, "error": "no such user"};
-exports.databaseError = {"code": 2, "error": databaseError};
+exports.databaseError = {"code": 2, "error": "databaseError"};
