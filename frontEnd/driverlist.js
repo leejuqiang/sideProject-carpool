a685b6ce9482ebbcfb05539b0b37e8ed8b2d14be
linjing0207 <lshao@dons.usfca.edu>
2018-12-11 15:15:55 -0800

    add passengerDriverList

--- frontEnd/driverlist.js
+++ frontEnd/driverlist.js
@@ -5,6 +5,8 @@ function selectAll(){
         chb.checked = true;   
     }
 }
+var postIDs = [];
+
 
 function getValueWhenChecked(){
     
@@ -14,27 +16,30 @@ function getValueWhenChecked(){
 
     var chbs = document.getElementsByName("chb[]");
     console.log(chbs.length);
-    // for (var i = 1; i < rows.length; i++) {
-    //     if(rows[i].cells[0].checked == true){
-    //         for(var j=1;j<rows[i].cells.length;j++){
-    //             console.log("row "+(i)+", colum"+(j)+": vlaue is "+rows[i].cells[j].innerHTML);
-    //         }
-    //     }
-    // }
     for (var i = 0; i < chbs.length; i++) {
         var chb = chbs[i];
         // console.log(rows[i + 2]);
         if(chb.checked == true){
-            
-            // for(var j=1;j<rows[i+2].cells.length;j++){
-            //     console.log("row "+(i+1)+", colum"+(j)+": vlaue is "+rows[i+2].cells[j].innerHTML);
-            // }
+            console.log("id");
+            console.log(tdriverPostList[i]._id);
+            postIDs.push(tdriverPostList[i]._id);
         } 
     }
+    console.log(postIDs);
+    // console.log(parent.document.getElementById("light"));
+    // document.getElementById("light").style.display='none';
+    // document.getElementById("fade").style.display='none'
+    // console.log(array);
+    // var passengerNumber = 1;
+    // var user = parent.runTimeData.user;
+    // console.log(user);
 
-    // var array = [{day:day, time: time, postID:[if empty, select all]}, {}];
-    // $.post("/repeatedApply", {"sessionID": string, "userID": string, "application": array}, function (data, s, xhr) {
-    //     server.database.
+    // var userid = "aldfjaslgadsg"; //user.userId;
+    // var session = "sdlf;sagjas"; //user.session;
+    // // var day = 1;
+    // // var time = 1;
+    // $.post("/repeatedApply", {"userID": userid, "sessionID": session, "postIDs": postIDs, "day": day, "time": time, "passengerNumber": passengerNum}, function (data, s, xhr) {
+    //     closeDialog();
     // }).fail(function (xhr, error, s) {
 
     // });
